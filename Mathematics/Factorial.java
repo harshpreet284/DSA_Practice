0e492a9110ea6f5787cac9978425be21c5fef00b
harshpreet284 <harshpreetsingh2643@gmail.com>
2026-08-01 10:57:39 +0530

    add

---
+++ Mathematics/Factorial.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int factorial(int n) {
+        // code here
+        int res = 1;
+        for(int i=2;i<=n;i++){
+            res = res * i;
+        }
+        return res;
+        
+    }
+}
---
+++ Mathematics/Trailing zeroes in factorial.java
@@ -0,0 +1,9 @@
+class Solution {
+    static int trailingZeroes(int n) {
+        int count = 0;
+        for(int i=5;i<=n;i=i*5){
+            count = count + n/i;
+        }
+        return count;
+    }
+}
