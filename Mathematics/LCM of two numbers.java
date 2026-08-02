d269068678efbd218a149478b3a2a45a5c0d4ac1
harshpreet284 <harshpreetsingh2643@gmail.com>
2026-08-02 10:41:13 +0530

    add

---
+++ Mathematics/LCM of two numbers.java
@@ -0,0 +1,11 @@
+class Solution {
+    int gcd( int a , int b){
+        if(b==0){
+            return a;
+        }
+        return gcd(b,a%b);
+    }
+    public int lcm(int a, int b) {
+        return (a*b/gcd(a,b));
+    }
+}
